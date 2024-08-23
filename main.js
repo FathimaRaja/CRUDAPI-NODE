caf4a3a820d2542e46ea281644a697779c9c32e5
Fathi <sa.shafee@gmail.com>
2024-08-23 13:05:34 +0530

    RouteStructure

--- main.js
+++ main.js
@@ -2,5 +2,17 @@ import express from "express";
 const app= express();
 const PORT=7000;
 app.get('/',(req,res)=>{ res.json({msg:"Hello students"});})
+//CRUD functionality of Products
+//Reading
+app.get('/Products',()=>{});
+//Creating or Adding
+app.post('/Products',()=>{});
+//Updating
+app.put('/Products/:id',()=>{});
+//Deleting
+app.delete('/Products/:id',()=>{});
+
+
+
 app.listen(PORT,()=>{console.log(`The server is running at http://localhost:${PORT}`);
 });
